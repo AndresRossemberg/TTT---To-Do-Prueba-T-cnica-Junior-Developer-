7a11c4ee4d16b574a80746ae00a4ad1c8af4eb92
AndresRossemberg <andres.rossemberg.m.d.l@gmail.com>
2025-11-05 23:10:02 -0500

    feat: Integración completa de Prisma y SQLite. Backend listo para persistencia.

--- backend-nest/src/tasks/tasks.controller.ts
+++ backend-nest/src/tasks/tasks.controller.ts
@@ -3,48 +3,36 @@ import {
   Get,
   Post,
   Body,
+  Patch,
   Param,
-  Put,
   Delete,
-  HttpCode,
-  HttpStatus,
-  ParseIntPipe,
 } from '@nestjs/common';
+
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
-import type { Task } from './task.interface';
-import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { Task } from '@prisma/client';
 
-@ApiTags('Tareas')
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Post()
-  @HttpCode(HttpStatus.CREATED)
-  @ApiOperation({ summary: 'Crea una nueva tarea' })
-  @ApiResponse({ status: 201, description: 'Tarea creada exitosamente.' })
-  create(@Body() createTaskDto: CreateTaskDto): Task {
+  async create(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
     return this.tasksService.create(createTaskDto);
   }
 
   @Get()
-  @ApiOperation({ summary: 'Obtiene la lista completa de tareas' })
-  @ApiResponse({ status: 200, description: 'Lista de tareas devuelta.' })
-  findAll(): Task[] {
+  async findAll(): Promise<Task[]> {
     return this.tasksService.findAll();
   }
 
-  @Put(':id/complete')
-  @ApiOperation({ summary: 'Marca una tarea como completada' })
-  updateComplete(@Param('id', ParseIntPipe) id: number): Task {
-    return this.tasksService.updateCompleted(id, true);
+  @Patch(':id/complete')
+  async complete(@Param('id') id: string): Promise<Task> {
+    return this.tasksService.complete(id);
   }
 
   @Delete(':id')
-  @HttpCode(HttpStatus.NO_CONTENT)
-  @ApiOperation({ summary: 'Elimina una tarea por ID' })
-  delete(@Param('id', ParseIntPipe) id: number): void {
-    return this.tasksService.delete(id);
+  async remove(@Param('id') id: string) {
+    return this.tasksService.remove(id);
   }
 }
